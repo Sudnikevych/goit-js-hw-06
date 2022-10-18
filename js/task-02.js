fd535e3997a13d28139f5e08fec81969b37c99f0
Sudnikevych <sudnikevych@gmail.com>
2022-10-18 15:53:50 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,23 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+const elements = ingredients.map((ingredient) => {
+  const liElement = document.createElement("li");
+
+  liElement.classList.add("item");
+  liElement.textContent = `${ingredient}`;
+
+  return liElement;
+});
+
+list.append(...elements);
+
+console.log(elements);
